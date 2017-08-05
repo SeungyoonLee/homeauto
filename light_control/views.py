4a29418aabcc47f45ea4b1b611baf407eb238f31
SeungyoonLee <samsee@ibksystem.co.kr>
2017-08-05 19:44:29 +0900

    Set urls

--- light_control/views.py
+++ light_control/views.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def light_list(request):
+    return render(request, 'light_control/light_list.html', {})
