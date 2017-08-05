bb585ca5b86eb490a700fb09d790b70d1bcf84aa
SeungyoonLee <samsee@ibksystem.co.kr>
2017-08-05 19:20:18 +0900

    Add model

--- light_control/models.py
+++ light_control/models.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class LightControl(models.Model):
+    location = models.CharField(max_length=100)
+    switch_on = models.BooleanField()
+    last_date = models.DateTimeField(default=timezone.now)
+
+    def on(self):
+        self.last_date = timezone.now()
+        self.switch_on = True
+        self.save()
+
+    def off(self):
+        self.last_date = timezone.now()
+        self.switch_on = False
+
+        self.save()
+
+    def __str__(self):
+        return self.location
